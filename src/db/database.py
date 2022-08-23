de95da85864f364ca5cd1c918889d14b6e762d02
Arif Garayev <garayevarif@gmail.com>
2022-08-23 13:38:30 +0400

    Update database.py

--- src/db/database.py
+++ src/db/database.py
@@ -2,7 +2,7 @@ import sqlalchemy as _sql
 import sqlalchemy.ext.declarative as _declarative
 import sqlalchemy.orm as _orm
 
-DATABASE_URL = 'postgresql://root:root@0.0.0.0:5432/postgres_db'
+DATABASE_URL = 'postgresql://root:root@postgres:5432/postgres_db'
 
 engine = _sql.create_engine(DATABASE_URL)
 
